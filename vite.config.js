1d8f94f755379c4ead221bf804094bc45a880f38
ahong996 <956628210@qq.com>
2022-03-29 21:26:12 +0800

    配置别名

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,8 +12,15 @@ export default defineConfig({
     AutoImport({
       resolvers: [ElementPlusResolver()],
     }),
+    // 按需加载，会自动导入组件
     Components({
       resolvers: [ElementPlusResolver()],
     }),
-  ]
+  ],
+  // 配置别名
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  }
 })
